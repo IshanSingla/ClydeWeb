6a487768e1153bafafb0bbf6e3f5bb1a0756425a
Ishan <is.ishan.singla@gmail.com>
2022-03-29 16:05:30 +0530

    ishan

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 from flask import Flask, flash, jsonify, request, render_template, send_file, redirect
-import firebase_admin, asyncio, os, requests, pyrebase
-from firebase_admin import db, credentials
+import firebase_admin
+import asyncio
+import os
+import requests
+import pyrebase
 from datetime import datetime, date
 from flask import *
 app = Flask(__name__)
@@ -12,24 +15,26 @@ config = {
     "storageBucket": "inducedmesssystem.appspot.com",
     "messagingSenderId": "1059758496051"
 }
+
+from firebase_admin import db, credentials
 cred = credentials.Certificate("1.json")
-default_app = firebase_admin.initialize_app(cred, {'databaseURL': "https://inducedmesssystem-default-rtdb.asia-southeast1.firebasedatabase.app/"})
+default_app = firebase_admin.initialize_app(
+    cred, {'databaseURL': "https://inducedmesssystem-default-rtdb.asia-southeast1.firebasedatabase.app/"})
 auth = pyrebase.initialize_app(config).auth()
 
 @app.route('/')
 def home():
-    print(request.remote_addr)
     email = request.cookies.get('email')
     password = request.cookies.get('password')
     try:
-        info=auth.sign_in_with_email_and_password(email, password)
-        details=auth.get_account_info(info["idToken"])
-        if details["users"][0]["emailVerified"]!= False:
+        info = auth.sign_in_with_email_and_password(email, password)
+        details = auth.get_account_info(info["idToken"])
+        if details["users"][0]["emailVerified"] == True:
             proxys = (db.reference(f"/Details/{info['localId']}/")).get()
             return render_template('index.html', s=proxys["Name"][0])
             return render_template('index.html', s=proxys["Name"])
     except Exception as e:
-        pass 
+        pass
     return render_template('index.html')
     return render_template('login.html')
 
@@ -38,25 +43,28 @@ def basic():
     email1 = request.cookies.get('email')
     password1 = request.cookies.get('password')
     try:
-        info=auth.sign_in_with_email_and_password(email1, password1)
-        details=auth.get_account_info(info["idToken"])
-        if details["users"][0]["emailVerified"]!= False:
-           return redirect("/")
+        info = auth.sign_in_with_email_and_password(email1, password1)
+        details = auth.get_account_info(info["idToken"])
+        if details["users"][0]["emailVerified"] != False:
+            return redirect("/")
     except:
-        pass 
+        pass
     if request.method == 'POST':
         try:
             name = request.form['name']
             email = request.form['email']
             password = request.form['password']
             try:
-                newuser=auth.create_user_with_email_and_password(email,password)
+                newuser = auth.create_user_with_email_and_password(
+                    email, password)
                 auth.send_email_verification(newuser["idToken"])
-                proxys = (db.reference(f"/Details/{info['localId']}/Name")).set(name)
-                details=auth.get_account_info(newuser["idToken"])
-                if details["users"][0]["emailVerified"]== False:
+                proxys = (db.reference(
+                    f"/Details/{info['localId']}/Name")).set(name)
+                details = auth.get_account_info(newuser["idToken"])
+                if details["users"][0]["emailVerified"] == False:
                     return render_template('login.html', s='Verify Your Email')
-                resp = make_response(render_template('login.html', s='Login successful'))
+                resp = make_response(render_template(
+                    'login.html', s='Login successful'))
                 resp.set_cookie('email', email, max_age=60*60*24)
                 resp.set_cookie('password', password, max_age=60*60*24)
                 return resp
@@ -67,11 +75,13 @@ def basic():
                 email = request.form['email1']
                 password = request.form['password1']
                 try:
-                    info=auth.sign_in_with_email_and_password(email, password)
-                    details=auth.get_account_info(info["idToken"])
-                    if details["users"][0]["emailVerified"]== False:
+                    info = auth.sign_in_with_email_and_password(
+                        email, password)
+                    details = auth.get_account_info(info["idToken"])
+                    if details["users"][0]["emailVerified"] == False:
                         return render_template('login.html', s='Verify Your Email')
-                    resp = make_response(render_template('login.html', s='Login successful'))
+                    resp = make_response(render_template(
+                        'login.html', s='Login successful'))
                     resp.set_cookie('email', email, max_age=60*60*24)
                     resp.set_cookie('password', password, max_age=60*60*24)
                     return resp
@@ -81,12 +91,14 @@ def basic():
                 pass
     return render_template('login.html', s="")
 
+
 @app.route('/logout')
 def logout():
     res = make_response(redirect("/login"))
     res.set_cookie('email', "", max_age=0)
     res.set_cookie('password', "", max_age=0)
     return res
+
 
 @app.route('/passreset', methods=['GET', 'POST'])
 def passreset():
@@ -99,24 +111,29 @@ def passreset():
             return render_template('new.html', us='Email Not Found')
     return render_template('new.html')
 
+
 @app.route('/tutor')
 def tutor():
     return render_template("tutor.html")
 
+
 @app.route('/cources')
 def courses():
     return render_template("cources.html")
+
 
 @app.route('/cources/<string:n>')
 def cources(n):
     proxys = (db.reference(f"/Courses/{n}/")).get()
     if proxys == None:
         return render_template("404.html")
-    return render_template("courcestemp.html",data=proxys)
+    return render_template("courcestemp.html", data=proxys)
+
 
 @app.route('/doubt')
 def doubt():
     return render_template("doubt.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True, threaded=True)
